c8e31ee5b82532860da71b955295b4b209aae30e
ahermanoml <mascarenhasluz@gmail.com>
2023-07-24 05:44:19 -0300

    -A

--- myScript.js
+++ myScript.js
@@ -3,9 +3,12 @@ let  container = document.querySelector("#container");
 for (let i =0; i < 16; i++) {
 let lineDiv = document.createElement("div");
     for (let j =0; j < 16; j++) {
-    let columnDiv = document.createElement("div")
-    columnDiv.classList.add("square")
-    lineDiv.appendChild(columnDiv)
+    let square = document.createElement("div")
+    square.classList.add("square")
+    square.addEventListener("mouseover", function() {
+        square.classList.add('hoverSquare')
+    })
+    lineDiv.appendChild(square)
     }
 lineDiv.classList.add("line")
 container.appendChild(lineDiv)

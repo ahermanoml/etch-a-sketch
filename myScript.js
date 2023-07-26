9faa94d351c521bda77ae5e83d7b13b1cc46f0a0
ahermanoml <mascarenhasluz@gmail.com>
2023-07-26 06:31:10 -0300

    -A

--- myScript.js
+++ myScript.js
@@ -1,12 +1,33 @@
 let button = document.querySelector('#reset')
+let  container = document.querySelector("#container");
+
+function drawGrid () {
+    for (let i =0; i < 16; i++) {
+        let lineDiv = document.createElement("div");
+            for (let j =0; j < 16; j++) {
+            let square = document.createElement("div")
+            square.classList.add("square")
+            square.addEventListener("mouseover", function() {
+                square.classList.add('hoverSquare')
+            })
+            lineDiv.appendChild(square)
+            }
+        lineDiv.classList.add("line")
+        container.appendChild(lineDiv)
+        }
+}
+
+drawGrid();
+
+let newContainer;
 
 button.addEventListener("click", function () {
     let userNumber = prompt('Choose the size of the grid');
     if (userNumber == 10) {
-        button.nextElementSibling.remove();
-        let newContainer = document.createElement('div');
+        container.remove();
+        newContainer = document.createElement('div');
         document.body.appendChild(newContainer)
-        newContainer.id = 'container';
+        newContainer.class = 'newContainer';
         for (let i =0; i < 16; i++) {
             let lineDiv = document.createElement("div");
                 for (let j =0; j < 16; j++) {
@@ -23,21 +44,8 @@ button.addEventListener("click", function () {
     }
 });
 
-let  container = document.querySelector("#container");
 
-for (let i =0; i < 16; i++) {
-        let lineDiv = document.createElement("div");
-            for (let j =0; j < 16; j++) {
-            let square = document.createElement("div")
-            square.classList.add("square")
-            square.addEventListener("mouseover", function() {
-                square.classList.add('hoverSquare')
-            })
-            lineDiv.appendChild(square)
-            }
-        lineDiv.classList.add("line")
-        container.appendChild(lineDiv)
-        }
+
 
 
 /*function createGrid () {

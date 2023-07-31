345e1d1a796e72135b629de0bdf4b40f53285539
ahermanoml <mascarenhasluz@gmail.com>
2023-07-31 06:20:37 -0300

    -A

--- myScript.js
+++ myScript.js
@@ -20,19 +20,20 @@ function drawGrid () {
         }
 }
 
+
 drawGrid();
 
 
 button.addEventListener("click", function () {
     let userNumber = prompt('Choose the size of the grid');
-    if (userNumber == 10) {
+    if (userNumber < 100) {
         container.remove();
         container = document.createElement('div');
         container.classList.add("container")
         document.body.appendChild(container)
-        for (let i =0; i < 16; i++) {
+        for (let i =0; i < userNumber; i++) {
             let lineDiv = document.createElement("div");
-                for (let j =0; j < 16; j++) {
+                for (let j =0; j < userNumber; j++) {
                 let square = document.createElement("div")
                 square.classList.add("square")
                 square.addEventListener("mouseover", function() {
@@ -42,28 +43,15 @@ button.addEventListener("click", function () {
                 }
             lineDiv.classList.add("line")
             container.appendChild(lineDiv)
-            }
+            } 
+    } else if (userNumber > 99) {
+        alert('Numbers bigger than 100 are not accepted');
+    } else {
+        alert ('Invalid Input');
     }
+
+    
 });
 
-
-
-
-
-/*function createGrid () {
-    for (let i =0; i < 16; i++) {
-        let lineDiv = document.createElement("div");
-            for (let j =0; j < 16; j++) {
-            let square = document.createElement("div")
-            square.classList.add("square")
-            square.addEventListener("mouseover", function() {
-                square.classList.add('hoverSquare')
-            })
-            lineDiv.appendChild(square)
-            }
-        lineDiv.classList.add("line")
-        newContainer.appendChild(lineDiv)
-        }
-}*/
 
 
